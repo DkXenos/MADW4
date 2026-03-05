74b2211efa6929184ed67ef2b4f64a4a39a9353f
DkXenos <superjason101010@gmail.com>
2026-03-05 22:47:37 +0700

    init files

---
+++ Lab_Week_4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lab_Week_4
+//
+//  Created by Jason TIo on 05/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lab_Week_4/Lab_Week_4App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab_Week_4App.swift
+//  Lab_Week_4
+//
+//  Created by Jason TIo on 05/03/26.
+//
+
+import SwiftUI
+
+@main
+struct Lab_Week_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
